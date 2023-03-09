c05da3dc0a04da3dc953204fc61d93be17e64661
Michael McNiel <michaelmcniel65@gmail.com>
2023-03-09 17:12:39 -0600

    Labeled HTML elements, deleted media query

--- src/home.jsx
+++ src/home.jsx
@@ -5,24 +5,30 @@ import './index.css';
 function ProfileSideBar() {
   return (
     <>
-    <div className='profileBarLarge'>
-      <div className='flex flex-col justify-between bg-white min-h-screen w-96'>
-        <div className='flex flex-col items-center gap-10'>
-          <img className='pt-5' src="./burgerbinassets/burgerbinlogo.png"/>
-          <h1 className='welcome-user text-3xl text-black pt-4'>Welcome, Bartholomew!</h1>
-          <img className='w-[20rem] h-[20rem] rounded-full object-cover' 
-              src='./burgerbinassets/defaultprofileimage.jpg'/>
-          <button className='w-[20rem] bg-blue-500 hover:bg-blue-400 text-white font-bold 
-            py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-3xl'>Profile</button>
-          <button className='w-[20rem] bg-yellow-500 hover:bg-yellow-400 text-white font-bold 
-            py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded-3xl'>Friends</button>
-          <button className='w-[20rem] bg-green-500 hover:bg-green-400 text-white font-bold 
-            py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded-3xl'>Games</button>
-        </div>
-        <div className='flex flex-col items-center p-5'>
-        <button className='bg-red-500 hover:bg-red-400 text-white font-bold 
-            py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded-3xl'>Logout</button>
-        </div>
+    <div className='white-background flex flex-col justify-between bg-white min-h-screen w-96'>
+      <div className='first-section flex flex-col items-center gap-10'>
+        <img className='website-logo pt-5' 
+          src="./burgerbinassets/burgerbinlogo.png"
+        />
+        <h1 className='welcome-user text-3xl text-black pt-4'>Welcome, Bartholomew!
+        </h1>
+        <img className='profile-picture w-[20rem] h-[20rem] rounded-full object-cover' 
+            src='./burgerbinassets/defaultprofileimage.jpg'
+        />
+        <button className='profile-button w-[20rem] bg-blue-500 hover:bg-blue-400 text-white font-bold 
+          py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-3xl'>Profile
+        </button>
+        <button className='friends-button w-[20rem] bg-yellow-500 hover:bg-yellow-400 text-white font-bold 
+          py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded-3xl'>Friends
+        </button>
+        <button className='games-button w-[20rem] bg-green-500 hover:bg-green-400 text-white font-bold 
+          py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded-3xl'>Games
+        </button>
+      </div>
+      <div className='second-section flex flex-col items-center p-5'>
+      <button className='logout-button bg-red-500 hover:bg-red-400 text-white font-bold 
+          py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded-3xl'>Logout
+        </button>
       </div>
     </div>
     </>
