05da7e22bd5264d1afb1bae98c266e08ca38468e
Michael McNiel <michaelmcniel65@gmail.com>
2023-03-09 19:27:09 -0600

    Added new pages

--- src/home.jsx
+++ src/home.jsx
@@ -21,24 +21,28 @@ function ProfileSideBar() {
         <button className='profile-button w-[20rem] bg-blue-500 hover:bg-blue-400 text-white font-bold 
           py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-3xl lg:hidden'>Profile
         </button>
-        <button className='profile-button-small bg-blue-500 hover:bg-blue-400 w-[3rem] h-[3rem] rounded-full xl:hidden'><i className="fa fa-address-card" aria-hidden="true"/></button>
+        <button className='profile-button-small bg-blue-500 hover:bg-blue-400 w-[3rem] h-[3rem] rounded-full xl:hidden'>
+          <i className="fa fa-address-card" aria-hidden="true"/></button>
         {/*Friends button*/}
         <button className='friends-button w-[20rem] bg-yellow-500 hover:bg-yellow-400 text-white font-bold 
           py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded-3xl lg:hidden'>Friends
         </button>
-        <button className='profile-button-small bg-yellow-500 hover:bg-yellow-400 w-[3rem] h-[3rem] rounded-full xl:hidden'><i className="fa fa-users" aria-hidden="true"/></button>
+        <button className='profile-button-small bg-yellow-500 hover:bg-yellow-400 w-[3rem] h-[3rem] rounded-full xl:hidden'>
+          <i className="fa fa-users" aria-hidden="true"/></button>
         {/*Games button*/}
         <button className='games-button w-[20rem] bg-green-500 hover:bg-green-400 text-white font-bold 
           py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded-3xl lg:hidden'>Games
         </button>
-        <button className='profile-button-small bg-green-500 hover:bg-green-400 w-[3rem] h-[3rem] rounded-full xl:hidden'><i className="fa fa-gamepad" aria-hidden="true"/></button>
+        <button className='profile-button-small bg-green-500 hover:bg-green-400 w-[3rem] h-[3rem] rounded-full xl:hidden'>
+          <i className="fa fa-gamepad" aria-hidden="true"/></button>
       </div>
       <div className='second-section flex flex-col items-center p-5'>
         {/*Logout button*/}
         <button className='logout-button bg-red-500 hover:bg-red-400 text-white font-bold 
             py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded-3xl lg:hidden'>Logout
         </button>
-        <button className='profile-button-small bg-red-500 hover:bg-red-400 w-[3rem] h-[3rem] rounded-full xl:hidden'><i className="fa fa-caret-square-o-down" aria-hidden="true"/></button>
+        <button className='profile-button-small bg-red-500 hover:bg-red-400 w-[3rem] h-[3rem] rounded-full xl:hidden'>
+          <i className="fa fa-caret-square-o-down" aria-hidden="true"/></button>
       </div>
     </div>
     </>
@@ -46,7 +50,7 @@ function ProfileSideBar() {
 }
 
 
-function contentScroll() {
+function Content() {
   return (
     <>
 
@@ -60,8 +64,7 @@ export default function Home() {
     <>
     <div className='flex'>
       <ProfileSideBar/>
-      <div className='w-[300px]'></div>
-      <div></div>
+      <button ></button>
     </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './home'
+import Home from './Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
