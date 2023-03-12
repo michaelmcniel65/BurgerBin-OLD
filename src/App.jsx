fc3528297da5212eebd4e7757e5c1c6e2a4c1792
Michael McNiel <michaelmcniel65@gmail.com>
2023-03-11 20:43:43 -0600

    Changed navbar, added responsivness

--- src/App.jsx
+++ src/App.jsx
@@ -30,49 +30,59 @@ const Root = () => {
     return (
         <>
         {/*White background and logo*/}
-        <div className='white-background flex flex-row bg-white h-20'>
-          <div className='first-section flex flex-row'>
+        <div className='white-background flex flex-row w-full bg-white h-20 justify-between'>
+          <div className='first-section flex flex-row items-center'>
             <Link to='/'>
-            <img className='website-logo p-5' 
+            <img className='website-logo p-3 w-full h-auto' 
               src="./burgerbinassets/burgerbinlogo.png"/>
             </Link>
-  
-            {/*Welcome message and profile picture*/}
-            <Link to='/'>
-            <img className='profile-picture w-[4rem] lg:w-[3.5rem] h-[4rem] lg:h-[3.5rem] lg:p-1 rounded-full object-cover' src='./burgerbinassets/defaultprofileimage.jpg'/>
-            </Link>
-  
+          </div>
+
+          <div className='flex flex-row w-[40rem] justify-evenly items-center'>
             {/*Profile button*/}
+            
             <Link to='/profile'>
-            <button className='profile-button w-[20rem] bg-blue-500 hover:bg-blue-400 text-white font-bold 
+            <button className='profile-button w-[10rem] bg-blue-500 hover:bg-blue-400 text-white font-bold 
               py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-3xl lg:hidden'>Profile
             </button>
-            <button className='profile-button-small bg-blue-500 hover:bg-blue-400 w-[3rem] h-[3rem] rounded-full xl:hidden'>
+            <button className='profile-button-small bg-blue-500 hover:bg-blue-400 w-[4rem] h-[3rem] rounded-full xl:hidden'>
               <i className="fa fa-address-card" aria-hidden="true"/>
             </button>
             </Link>
   
             {/*Friends button*/}
             <Link to='/friends'>
-            <button className='friends-button w-[20rem] bg-yellow-500 hover:bg-yellow-400 text-white font-bold 
+            <button className='friends-button w-[10rem] bg-yellow-500 hover:bg-yellow-400 text-white font-bold 
               py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded-3xl lg:hidden'>Friends
             </button>
-            <button className='profile-button-small bg-yellow-500 hover:bg-yellow-400 w-[3rem] h-[3rem] rounded-full xl:hidden'>
+            <button className='profile-button-small bg-yellow-500 hover:bg-yellow-400 w-[4rem] h-[3rem] rounded-full xl:hidden'>
               <i className="fa fa-users" aria-hidden="true"/>
             </button>
             </Link>
   
             {/*Games button*/}
             <Link to='/games'>
-            <button className='games-button w-[20rem] bg-green-500 hover:bg-green-400 text-white font-bold 
+            <button className='games-button w-[10rem] bg-green-500 hover:bg-green-400 text-white font-bold 
               py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded-3xl lg:hidden'>Games
             </button>
-            <button className='profile-button-small bg-green-500 hover:bg-green-400 w-[3rem] h-[3rem] rounded-full xl:hidden'>
+            <button className='profile-button-small bg-green-500 hover:bg-green-400 w-[4rem] h-[3rem] rounded-full xl:hidden'>
               <i className="fa fa-gamepad" aria-hidden="true"/>
             </button>
             </Link>
           </div>
 
+          <div className='flex flex-row w-[15rem] justify-evenly items-center'>
+            {/*Logout button*/}
+            <button className='logout-button bg-red-500 hover:bg-red-400 text-white font-bold 
+                py-2 px-4 w-[6rem] h-[3rem] border-b-4 border-red-700 hover:border-red-500 rounded-3xl lg:hidden'>Logout
+            </button>
+            <button className='profile-button-small bg-red-500 hover:bg-red-400 w-[3rem] h-[3rem] rounded-full xl:hidden'>
+              <i className="fa fa-caret-square-o-down" aria-hidden="true"/></button>
+            {/*Welcome message and profile picture*/}
+            <Link to='/'>
+            <img className='profile-picture w-[4rem] lg:w-[3.5rem] h-[4rem] lg:h-[3.5rem] lg:p-1 rounded-full object-cover' src='./burgerbinassets/defaultprofileimage.jpg'/>
+            </Link>
+          </div>
         </div>
 
         <div>
@@ -84,12 +94,6 @@ const Root = () => {
 
 export default App
 
-{/* <div className='second-section flex flex-col items-center p-5'>
+
   
-{/*Logout button*/}
-{/* <button className='logout-button bg-red-500 hover:bg-red-400 text-white font-bold 
-    py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded-3xl lg:hidden'>Logout
-</button>
-<button className='profile-button-small bg-red-500 hover:bg-red-400 w-[3rem] h-[3rem] rounded-full xl:hidden'>
-  <i className="fa fa-caret-square-o-down" aria-hidden="true"/></button>
-</div> */}
+
